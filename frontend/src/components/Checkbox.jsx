93222ff7d5dbe661336a717c8a668ffb7024b88d
steven-kollo <steven.kollo.workspace@gmail.com>
2025-06-06 11:37:13 +0400

    pretify welcome

--- frontend/src/components/Checkbox.jsx
+++ frontend/src/components/Checkbox.jsx
@@ -1,6 +1,6 @@
 export default function Checkbox({ children, onChange, checked }) {
   return (
-    <label className="flex items-center justify-center gap-3 mt-2 text-[15px] text-gray-600 cursor-pointer mx-auto text-center leading-snug">
+    <label className="flex items-center justify-center gap-3 mt-1 text-[15px] text-gray-600 cursor-pointer mx-auto text-center leading-snug">
       <input
         type="checkbox"
         className="form-checkbox h-4 w-4 rounded border-gray-300 text-[#00B2FF] focus:ring-0"
