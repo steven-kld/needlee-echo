6f9575beb2f45069a47d3335314da8f762096efb
steven-kollo <steven.kollo.workspace@gmail.com>
2025-06-06 11:22:06 +0400

    pretify welcome

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -7,7 +7,6 @@ export default function Button({ children, onClick, icon = null, disabled = fals
         ${disabled
           ? 'bg-gradient-to-r from-[#9ACCF9] to-[#D0B9F9] text-white cursor-not-allowed'
           : 'bg-gradient-to-r from-[#00B2FF] to-[#C084FC] text-white hover:brightness-110'}
-        drop-shadow-[0_0_12px_rgba(192,132,252,0.5)]
       `}
     >
       {icon && <span className="text-xl">{icon}</span>}
